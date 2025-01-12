ca089357f419035155fa5bc859005324c301a994
Krishnan K <150646868+Krishnan005K@users.noreply.github.com>
2025-01-12 10:38:49 +0530

    Time: 708 ms (41.56%) | Memory: 0B (100.00%) - LeetSync

--- 608-tree-node/tree-node.sql
+++ 608-tree-node/tree-node.sql
@@ -1,8 +1,15 @@
-# Write your MySQL query statement below
-SELECT  id,
-    CASE 
-        WHEN p_id is null then 'Root'
-        WHEN id IN (SELECT p_id FROM Tree) THEN "Inner"
-        ELSE 'Leaf'
-        END AS type
-FROM Tree
+-- # Write your MySQL query statement below
+-- SELECT  id,
+--     CASE 
+--         WHEN p_id is null then 'Root'
+--         WHEN id IN (SELECT p_id FROM Tree) THEN "Inner"
+--         ELSE 'Leaf'
+--         END AS type
+-- FROM Tree
+
+select a.id,
+case when a.p_id is null then "Root"
+when b.p_id is not null then "Inner"
+when b.p_id is null then "Leaf" end as type
+from Tree a
+left join (select distinct p_id from Tree) b on a.id = b.p_id
