bc83b080e599d2f8b6ea014bb6db8b838fc6b863
Krishnan K <150646868+Krishnan005K@users.noreply.github.com>
2025-02-20 21:59:52 +0530

    Time: 50 ms (57.16%) | Memory: 45.3 MB (74.17%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,21 +1,38 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& arr, int k) {
+//         int n=arr.size();
+//         int cnt=0;
+//         for (int i = 0 ; i < n; i++) { // starting index i
+//             int sum = 0;
+//         for (int j = i; j < n; j++) { // ending index j
+
+//             // calculate the sum of subarray [i...j]
+//                 sum += arr[j];
+
+//             // Increase the count if sum == k:
+//             if (sum == k)
+//                 cnt++;
+//         }
+//     }
+//     return cnt;
+// }
+    
+// };
 class Solution {
 public:
-    int subarraySum(vector<int>& arr, int k) {
-        int n=arr.size();
-        int cnt=0;
-        for (int i = 0 ; i < n; i++) { // starting index i
-            int sum = 0;
-        for (int j = i; j < n; j++) { // ending index j
-
-            // calculate the sum of subarray [i...j]
-                sum += arr[j];
-
-            // Increase the count if sum == k:
-            if (sum == k)
-                cnt++;
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0;
+        int c = 0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(mp.find(sum-k)!=mp.end()){
+                c += mp[sum-k];
+            }
+            mp[sum]++;
         }
+        return c;
     }
-    return cnt;
-}
-    
 };
