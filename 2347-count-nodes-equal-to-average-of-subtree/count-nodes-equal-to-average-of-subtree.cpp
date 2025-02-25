8e619156c5c112ce55c6d360f23f23a9622c969c
Krishnan K <150646868+Krishnan005K@users.noreply.github.com>
2025-02-25 14:27:00 +0530

    Time: 31 ms (5.11%) | Memory: 16 MB (11.26%) - LeetSync

--- 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.cpp
@@ -1,37 +1,52 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     int sum(TreeNode * root,int & count)
+//     {
+//         if(!root)   return 0;
+//         count++;
+//         int left=sum(root->left,count);
+//         int right=sum(root->right,count);
+//         return (root->val + left+right);
+//     }
+//     void solve(TreeNode * root,int &ans)
+//     {
+//         if(!root) return ;
+//         int count=0;
+//         int avg=(sum(root,count))/count;
+//         if(avg==root->val)
+//             ans++;
+//         solve(root->left,ans);
+//         solve(root->right,ans);
+//     }
+//     int averageOfSubtree(TreeNode* root) {
+//         int ans=0;
+//         solve(root,ans);
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
-    int sum(TreeNode * root,int & count)
-    {
-        if(!root)   return 0;
-        count++;
-        int left=sum(root->left,count);
-        int right=sum(root->right,count);
-        return (root->val + left+right);
+    int cnt(TreeNode* root){
+        return !root ? 0 : 1 + cnt(root->left) + cnt(root->right);
     }
-    void solve(TreeNode * root,int &ans)
-    {
-        if(!root) return ;
-        int count=0;
-        int avg=(sum(root,count))/count;
-        if(avg==root->val)
-            ans++;
-        solve(root->left,ans);
-        solve(root->right,ans);
+    int sum(TreeNode* root){
+        return !root ? 0 : root->val + sum(root->left) + sum(root->right);
     }
     int averageOfSubtree(TreeNode* root) {
-        int ans=0;
-        solve(root,ans);
-        return ans;
+        if(!root)   return 0;
+        int ans = averageOfSubtree(root->left) + averageOfSubtree(root->right);
+        return sum(root) / cnt(root)==root->val ? ans + 1 : ans;
     }
 };
